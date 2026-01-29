4fed2c5ff9368f67a49dbfe015f19abb242335ad
kkottman <karlkottman@gmail.com>
2026-01-29 01:45:56 +0000

    HNY Function

---
+++ kkottmanFinalExamQ4.py
@@ -0,0 +1,11 @@
+#Karl Kottman
+#Final Exam Question 4
+
+
+def HNY(x):
+    for i in range(x):
+        print("Happy New Year!")
+
+x =int(input("Please enter a number between 1 and 10:"))
+HNY(x)
+
